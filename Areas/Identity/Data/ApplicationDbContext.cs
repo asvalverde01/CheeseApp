ca73fbb9917db5154b864f431f3a45fd25a63659
Alberto Valverde <77863587+asvalverde01@users.noreply.github.com>
2022-11-03 11:26:52 -0500

    Add project files.

---
+++ Areas/Identity/Data/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using CheeseApp.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CheeseApp.Areas.Identity.Data;
+
+public class ApplicationDbContext : IdentityDbContext<AppUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+
+        builder.ApplyConfiguration(new AppUserEntityConfiguration());
+    }
+
+    public class AppUserEntityConfiguration : IEntityTypeConfiguration<AppUser>
+    {
+        public void Configure(EntityTypeBuilder<AppUser> builder)
+        {
+            builder.Property(p => p.FirstName).HasMaxLength(50);
+            builder.Property(p => p.LastName).HasMaxLength(50);
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CheeseApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
