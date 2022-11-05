a375ef558731c02b998c797c19ee5f55b873b029
Alberto Valverde <77863587+asvalverde01@users.noreply.github.com>
2022-11-05 12:04:05 -0500

    Image

--- Migrations/20221105032321_post.Designer.cs
+++ Migrations/20221105032321_post.Designer.cs
@@ -4,6 +4,7 @@ using CheeseApp.Areas.Identity.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CheeseApp.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221105032321_post")]
+    partial class post
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -111,10 +113,6 @@ namespace CheeseApp.Migrations
                         .HasColumnType("nvarchar(100)");
 
                     b.Property<string>("FechaPost")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<string>("ImagenUrl")
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
---
+++ Models/Post.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Xml.Linq;
+
+namespace CheeseApp.Models
+{
+    public class Post
+    {
+        
+        public int ID { get; set; }
+        
+        [Required]
+        [Display(Name = "Fecha de publicación")]
+        public String? FechaPost { get; set; }
+
+        [Required]
+        [
+            StringLength(
+                100,
+                ErrorMessage = "Descripción no puede estar vacía",
+                MinimumLength = 2)
+        ]
+        public string? Descripcion { get; set; }
+
+        public string ImagenUrl { get; set; }
+        // Image type
+        [Required]
+        [Display(Name = "Subir una imagen ")]
+        [NotMapped]
+        public IFormFile ImagenFile { get; set; }
+
+    }
+}
